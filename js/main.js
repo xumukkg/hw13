00ff01d6e3e4991cc238eaa19d0ac7e1a0863e28
Nursultan <47150499+xumukkg@users.noreply.github.com>
2019-05-23 16:17:32 +0600

    ch 1

---
+++ js/main.js
@@ -0,0 +1,51 @@
+	$('#myModal').on('shown.bs.modal', function () {
+	$('#myInput').trigger('focus')
+});
+jQuery(function($){
+	// var elem = $('#box-1');
+	// elem.css('color', 'red');
+	// elem.css({
+	// 	'color': 'red',
+	// 	'font-weight': 'bold'
+	// })
+	
+// 	$('.boxes').find('.box')
+// 	           .css('textTransform','uppercase');
+// 	$('.boxes .box')
+// 				.eq(2)
+// 				.css('color','blue');
+// 	var str = 'hello world';
+// //	$('.box:nth-child(4)').text('hello world');
+// 	var titletext = $('.title').text('<h4>Lorem ipsum dolor sit.</h4>');
+// 	$('.box:nth-child(4)').text(titletext);
+// 	$('.boxes-2 .box:first-child').html('<div class="card">'+
+// 		'<h4>Lorem ipsum dolor sit.</h4>'
+// 		+'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>'+
+// 	+'</div>');
+// 	var newElem =$('<p>  Lorem</p>').css('color','red');
+// 	 $('.box').append(newElem);
+
+// 	 var newpara = $('<p> new text</p>').css('border-bottom','1px solid blue');
+
+// 	 $('.boxes-2 .box').after(newpara);
+
+
+// 	 $('#btn').on('click', function(){
+// 	 	If (!$('#modal').hasClass('is-open')) {
+// 	 		$('#modal').fadeIn(700)
+// 	 					.addClass('is-open');
+// 	 	} else {
+// 	 		$('#modal').fadeOut(400)
+// 	 		removeClass('is-open')
+
+// 	 	}	
+// 	 });
+// 	 console.log('you clicked button');
+//	 $('body').append(h4);
+
+
+
+
+
+ });
+
